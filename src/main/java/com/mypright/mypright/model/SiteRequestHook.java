09c3b0d5ef16c9a9036a1373f0c4d0fbfdd1fa00
AkhilG <akhilghatiki@INAkhilGs-MacBook-Pro.local>
2019-09-08 07:18:06 +0530

    Implement model to hook siteRequest with uniqueSiteId

---
+++ src/main/java/com/mypright/mypright/model/SiteRequestHook.java
@@ -0,0 +1,25 @@
+package com.mypright.mypright.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SiteRequestHook {
+
+  private String uniqueId;
+
+  private SiteRequest siteRequest;
+
+  @Override
+  public String toString() {
+    return "{" +
+        "uniqueId:'" + uniqueId + '\'' +
+        ", siteRequest:" + siteRequest +
+        '}';
+  }
+}
