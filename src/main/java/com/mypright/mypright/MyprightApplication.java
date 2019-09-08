b419f027c71fafe7772eefd520e6d586d6e73418
Mukarram Ali <mukarram.ali@thoughtworks.com>
2019-09-08 11:25:39 +0530

    Rest template bean

--- src/main/java/com/mypright/mypright/MyprightApplication.java
+++ src/main/java/com/mypright/mypright/MyprightApplication.java
@@ -2,12 +2,19 @@ package com.mypright.mypright;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MyprightApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyprightApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
