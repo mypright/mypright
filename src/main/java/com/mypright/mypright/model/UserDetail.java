4c21d025202801f96005e3bce9043febe4e26ef6
Akhil Ghatiki <akhil_ghatiki@gap.com>
2019-09-29 20:30:23 +0530

    Testing the postgre auto add on configuration in heroku

--- src/main/java/com/mypright/mypright/model/UserDetail.java
+++ src/main/java/com/mypright/mypright/model/UserDetail.java
@@ -1,16 +1,24 @@
 package com.mypright.mypright.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDetail {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
 
   private String detailName;
 
